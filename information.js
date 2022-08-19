f150057d0abb7d24bdfddab057b5386e163cbde9
JM <jm.jacquier@gmail.com>
2022-08-19 09:57:45 +0200

    final

--- information.js
+++ information.js
@@ -1,13 +1,6 @@
 const name = "Jean-Marie";
 const campus = "Lyon";
 
-function cowSay() {
-  console.log(`My passions are :`);
-  for (let i = 0; i < passions.length; i += 1) {
-    console.log(`- ${passions[i]}`);
-  }
-}
-
 module.exports = {
   name: name,
   campus: campus,
